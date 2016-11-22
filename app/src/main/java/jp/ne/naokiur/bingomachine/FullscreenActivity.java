b80aa47ef86e98dd2e49722d35506949456250f2
UruruSakuya <atomic.transfer.comp@gmail.com>
2016-11-22 20:28:27 +0900

    Delete redundant fields and Inner class.

--- app/src/main/java/jp/ne/naokiur/bingomachine/FullscreenActivity.java
+++ app/src/main/java/jp/ne/naokiur/bingomachine/FullscreenActivity.java
@@ -1,13 +1,11 @@
 package jp.ne.naokiur.bingomachine;
 
 import android.annotation.SuppressLint;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.MotionEvent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Calendar;
@@ -19,17 +17,6 @@ import jp.ne.naokiur.bingomachine.service.BingoNumber;
  * status bar and navigation/system bar) with user interaction.
  */
 public class FullscreenActivity extends AppCompatActivity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
 
     /**
      * Some older devices needs a small delay between UI widget updates
@@ -74,20 +61,6 @@ public class FullscreenActivity extends AppCompatActivity {
             hide();
         }
     };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
 
     private class RenderingRunnable implements Runnable {
         private final TextView targetView;
@@ -102,23 +75,6 @@ public class FullscreenActivity extends AppCompatActivity {
         public void run() {
             targetView.setText(text);
             targetView.invalidate();
-
-        }
-    }
-
-    private class AddingRunnable implements Runnable {
-        private final LinearLayout parentView;
-        private final TextView targetView;
-
-        private AddingRunnable(LinearLayout parentView, TextView targetView) {
-            this.parentView = parentView;
-            this.targetView = targetView;
-        }
-
-        @Override
-        public void run() {
-            this.parentView.addView(targetView);
-            this.parentView.invalidate();
 
         }
     }

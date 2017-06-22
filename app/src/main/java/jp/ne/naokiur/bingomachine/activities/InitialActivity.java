5b81a8ba1ba118fcb688c307893b762433521850
naokiur <atomic.transfer.comp@gmail.com>
2017-06-22 21:42:51 +0900

    Remove redundant source code.

--- app/src/main/java/jp/ne/naokiur/bingomachine/activities/InitialActivity.java
+++ app/src/main/java/jp/ne/naokiur/bingomachine/activities/InitialActivity.java
@@ -5,35 +5,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.InterstitialAd;
 
 import jp.ne.naokiur.bingomachine.R;
-import jp.ne.naokiur.bingomachine.strage.DatabaseHelper;
 
 public class InitialActivity extends AppCompatActivity implements BeginningFragment.OnFragmentInteractionListener {
     private InterstitialAd interstitialAd;
-
-    private EditText title;
-    private EditText maxNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
 
-        interstitialAd = new InterstitialAd(this);
-        interstitialAd.setAdUnitId(getString(R.string.banner_ad_unit_id));
-        requestNewInterstitial();
-
-        title = (EditText) findViewById(R.id.edit_title);
-        maxNumber = (EditText) findViewById(R.id.edit_max_number);
-
-        DatabaseHelper helper = new DatabaseHelper(this);
-        helper.getWritableDatabase();
+        loadInterstitial();
 
         Button button = (Button) findViewById(R.id.button_new_game);
         button.setOnClickListener(new View.OnClickListener() {
@@ -42,33 +29,21 @@ public class InitialActivity extends AppCompatActivity implements BeginningFragm
                 if (interstitialAd.isLoaded()) {
                     interstitialAd.show();
                     requestNewInterstitial();
-
                 }
+            }
+        });
+    }
 
-                interstitialAd.setAdListener(new AdListener() {
-                    @Override
-                    public void onAdClosed() {
-                        requestNewInterstitial();
-                        BeginningFragment.newInstance().show(getSupportFragmentManager(), "dialog");
-                    }
-                });
-//                String titleText = title.getText().toString();
-//                String maxNumberText = maxNumber.getText().toString();
-//
-//                if (!StringUtils.isNumeric(maxNumberText) || maxNumberText.length() > 3) {
-//                    displayToast(getString(R.string.message_error_invalid_max_number));
-//                    return;
-//                }
-//
-//                BingoGame bingoGame = new BingoGame(titleText, Integer.valueOf(maxNumberText), getBaseContext());
-//                try {
-//                    bingoGame.register();
-//                } catch (ValidateException e) {
-//                    displayToast(e.getMessage());
-//                    return;
-//                }
-//
+    private void loadInterstitial() {
+        interstitialAd = new InterstitialAd(this);
+        interstitialAd.setAdUnitId(getString(R.string.banner_ad_unit_id));
+        requestNewInterstitial();
 
+        interstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                requestNewInterstitial();
+                BeginningFragment.newInstance().show(getSupportFragmentManager(), "dialog");
             }
         });
     }
